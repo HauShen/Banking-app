193b43a9a2c1699fb086b328bc2f8338abd28977
HauShen <wukong_1@hotmail.com>
2026-04-25 23:45:25 +0800

    d

--- src/main/resources/db/migration/V1__user_table_create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id VARCHAR PRIMARY KEY,
-    role VARCHAR(20) NOT NULL,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-);
