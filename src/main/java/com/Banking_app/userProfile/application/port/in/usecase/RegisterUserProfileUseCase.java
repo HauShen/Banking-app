cd713725db9123423760771caf05ea82157671cf
Hau <haushen@optimo.com.my>
2026-08-19 16:46:14 +0800

    hexagonal_part3_added_registeruserprofileusecase_for_port_in

--- src/main/java/com/Banking_app/userProfile/application/port/in/usecase/RegisterUserProfileUseCase.java
+++ src/main/java/com/Banking_app/userProfile/application/port/in/usecase/RegisterUserProfileUseCase.java
@@ -1,5 +1,8 @@
 package com.Banking_app.userProfile.application.port.in.usecase;
 
-public class RegisterUserProfileUseCase {
+import com.Banking_app.userProfile.application.port.in.command.RegisterUserProfileCommand;
+import com.Banking_app.userProfile.domain.models.userprofile.UserProfile;
 
+public interface RegisterUserProfileUseCase {
+    UserProfile register(RegisterUserProfileCommand command);
 }
